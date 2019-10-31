40e6dbf9600ccc8fa8b41a2d66c929139e6fb52d
mko <gogs@fake.local>
2019-10-31 12:56:28 +0100

    Person2

---
+++ CS/Person2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS
+{
+    class Person2
+    {
+        Person2()
+        {
+            FirstName = "John";
+        }
+
+        string FirstName { get; }
+        string LastName { get; } = "Doe";
+        IList<string> Ids { get; } = new List<string>() { "abc123" };
+    }
+}
