68ddf2a6dc3170230942054d5b68eb1b7040ae62
Michał Kołodziejczyk <mikolodziejczyk77@gmail.com>
2022-03-30 15:34:40 +0200

    VSC

---
+++ CS/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS
+{
+    class Person
+    {
+        public Person()
+        {
+            FirstName = "John";
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; } = "Doe";
+        public IList<string> Ids { get; } = new List<string> { };
+    }
+}
